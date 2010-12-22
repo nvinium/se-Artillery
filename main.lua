54376caba506984b896e2c0bd15fc4e4a1a5a241
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2010-12-22 17:13:25 +0800

    - you can now fire more bullets (pewpewpewpew) - removed pre-rendered / pre-initialized bullet - bullets are now instantiated on screen tap

--- main.lua
+++ main.lua
@@ -7,21 +7,17 @@ local canon_size = 20
 local canon = display.newRect( display.contentWidth/2, display.contentHeight - canon_size, canon_size, canon_size )
 canon:setFillColor( 100, 200, 300 )
 
-local bullet = display.newCircle( display.contentWidth/2 + canon_size/2, 450, 5 )
-bullet:setFillColor( 150, 100, 300 )
 
-physics.addBody( bullet, "dynamic", { density=1, radius = 3, bounce=.7, friction=.5 } )
-bullet.isBullet = true
-bullet.isAwake = false
 
 local planets = {}
+local bullets = {}
+
 for planet_count=1, 3 do 
 	local planet = display.newCircle( math.random(0, display.contentWidth), math.random(0, display.contentHeight), 20 )
 	planet:setFillColor( math.random(0,255), math.random(0,255), math.random(0,255) )
 	physics.addBody( planet, "static", { friction=0.5, radius=20, friction=.9 } )
 	planets[#planets + 1] = planet
 end
-
 
 function distance( a, b )
 	local width, height = b.x-a.x, b.y-a.y
@@ -36,53 +32,71 @@ local ymove = -.5
 local push = -.01
 
 function doPlanet( event )
-	if bullet.isAwake  then
-		
-		for i=1, #planets do 
-			local planet = planets[i]
-			local dist 	= distance( planet, bullet )
-			local power = tmax/dist / strength
-			local xmove = (planet.x-bullet.x) * power
-			local ymove = (planet.y-bullet.y) * power
+	for i=1, #bullets do
+		bullet = bullets[i]
+		if bullet.isAwake then
+			dist = 0
+			power = 0 
+			xmove = 0 
+			ymove = 0
+			for j=1, #planets do 
+				planet = planets[j]
+				dist 	= distance( planet, bullet )
+				power = tmax/dist / strength
+				xmove = (planet.x-bullet.x) * power
+				ymove = (planet.y-bullet.y) * power
 
-			-- 
-			-- print("xmove is : ".. xmove)
-			-- print("ymove is : ".. ymove)
-			-- print("power is : ".. power)
-			-- print("--------")
-			bullet:applyForce( xmove, ymove+push, bullet.x, bullet.y )
-			draw_trail()
+				-- print("xmove is : ".. xmove)
+				-- print("ymove is : ".. ymove)
+				-- print("power is : ".. power)
+				-- print("--------")
+				bullet:applyForce( xmove, ymove+push, bullet.x, bullet.y )
+				draw_trail()
+			end
 		end
-
-		-- bullet:applyForce( xmove, ymove, planet.x, planet.y )
 	end
 end
 
 function draw_trail()
-	trail = display.newCircle( bullet.x, bullet.y, 1 )
-	trail:setFillColor(240, 200, 190)
+	for i=1, #bullets do 
+		bullet = bullets[i]
+		j = #bullet.trails+1
+		bullet.trails[j] = display.newCircle( bullet.x, bullet.y, 1 )
+		bullet.trails[j]:setFillColor(240, 200, 190)
+		if #bullet.trails > 100 then
+			bullet.trails[j-100]:removeSelf()
+		end
+	end
 end
 
 function fireCanon(event)
-	bullet.isAwake = true
+	i = #bullets + 1
+	bullets[i] = display.newCircle( display.contentWidth/2 + canon_size/2, 450, 5 )
+	bullet = bullets[i]
+	bullet.trails = {}
+	bullet:setFillColor( 150, 100, 300 )
+	physics.addBody( bullet, "dynamic", { density=1, radius = 3, bounce=.7, friction=.5 } )
+	bullet.isBullet = true
+	bullet.collision = onBulletCollision
+	bullet:addEventListener( "collision", bullet )
+	
 	print( "Bullet Fired!" )
-		touch_dist = distance( event, bullet )
-		init_power = tmax/touch_dist / strength
-		xmove = (event.x-bullet.x) * init_power
-		ymove = (event.y-bullet.y) * init_power
-		bullet:applyLinearImpulse( xmove, ymove, bullet.x, bullet.y )
+	touch_dist = distance( event, bullet )
+	init_power = tmax/touch_dist / strength
+	xmove = (event.x-bullet.x) * init_power
+	ymove = (event.y-bullet.y) * init_power
+	bullet:applyLinearImpulse( xmove, ymove, bullet.x, bullet.y )
 end
 
-function onBulletCollision(event)
+function onBulletCollision(self, event)
 	print("boom")
 	if event.phase == 'began' then
-		colissionMark = display.newCircle( bullet.x, bullet.y, 5 )
+		colissionMark = display.newCircle( self.x, self.y, 5 )
 		colissionMark:setFillColor( 255, 0, 0 )
 	elseif event.phase == 'ended' then
-		print( "Colission at X,Y : ".. bullet.x.. "," ..bullet.y )
+		print( "Ball ".. "Colission at X,Y : ".. self.x.. "," ..self.y )
 	end
 end
 
 Runtime:addEventListener( "tap", fireCanon )
-bullet:addEventListener( "collision", onBulletCollision )
 Runtime:addEventListener( "enterFrame", doPlanet )
