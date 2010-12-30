866b935dbd5fe9163179a555609654e2c80391ea
Radamanthus Batnag <rad@batnag.org>
2010-12-30 12:55:50 +0800

    Initial commit of menu using Director class (game.lua not yet properly integrated)

---
+++ game.lua
@@ -0,0 +1,109 @@
+local physics = require( "physics" )
+
+module(..., package.seeall)
+
+-- Main function - MUST return a display.newGroup()
+function new()
+	
+	physics.start()
+	physics.setGravity( 0, 0 )
+
+	local canon_size = 20
+	local canon = display.newRect( display.contentWidth/2, display.contentHeight - canon_size, canon_size, canon_size )
+	canon:setFillColor( 100, 200, 300 )
+
+
+
+	local planets = {}
+	local bullets = {}
+
+	for planet_count=1, 3 do 
+		local planet = display.newCircle( math.random(0, display.contentWidth), math.random(0, display.contentHeight), 20 )
+		planet:setFillColor( math.random(0,255), math.random(0,255), math.random(0,255) )
+		physics.addBody( planet, "static", { friction=0.5, radius=20, friction=.9 } )
+		planets[#planets + 1] = planet
+	end
+
+	function distance( a, b )
+		local width, height = b.x-a.x, b.y-a.y
+		return math.sqrt( width*width + height*height )
+	end
+
+	local tmax = distance( {x=0, y=0}, {x=display.contentWidth, y=display.contentHeight} )
+	local strength = 10000
+
+	local xmove = 0
+	local ymove = -.5
+	local push = -.01
+
+	function doPlanet( event )
+		for i=1, #bullets do
+			bullet = bullets[i]
+			if bullet.isAwake then
+				dist = 0
+				power = 0 
+				xmove = 0 
+				ymove = 0
+				for j=1, #planets do 
+					planet = planets[j]
+					dist 	= distance( planet, bullet )
+					power = tmax/dist / strength
+					xmove = (planet.x-bullet.x) * power
+					ymove = (planet.y-bullet.y) * power
+
+					-- print("xmove is : ".. xmove)
+					-- print("ymove is : ".. ymove)
+					-- print("power is : ".. power)
+					-- print("--------")
+					bullet:applyForce( xmove, ymove+push, bullet.x, bullet.y )
+					draw_trail()
+				end
+			end
+		end
+	end
+
+	function draw_trail()
+		for i=1, #bullets do 
+			bullet = bullets[i]
+			j = #bullet.trails+1
+			bullet.trails[j] = display.newCircle( bullet.x, bullet.y, 1 )
+			bullet.trails[j]:setFillColor(240, 200, 190)
+			if #bullet.trails > 100 then
+				bullet.trails[j-100]:removeSelf()
+			end
+		end
+	end
+
+	function fireCanon(event)
+		i = #bullets + 1
+		bullets[i] = display.newCircle( display.contentWidth/2 + canon_size/2, 450, 5 )
+		bullet = bullets[i]
+		bullet.trails = {}
+		bullet:setFillColor( 150, 100, 300 )
+		physics.addBody( bullet, "dynamic", { density=1, radius = 3, bounce=.7, friction=.5 } )
+		bullet.isBullet = true
+		bullet.collision = onBulletCollision
+		bullet:addEventListener( "collision", bullet )
+	
+		print( "Bullet Fired!" )
+		touch_dist = distance( event, bullet )
+		init_power = tmax/touch_dist / strength
+		xmove = (event.x-bullet.x) * init_power
+		ymove = (event.y-bullet.y) * init_power
+		bullet:applyLinearImpulse( xmove, ymove, bullet.x, bullet.y )
+	end
+
+	function onBulletCollision(self, event)
+		print("boom")
+		if event.phase == 'began' then
+			colissionMark = display.newCircle( self.x, self.y, 5 )
+			colissionMark:setFillColor( 255, 0, 0 )
+		elseif event.phase == 'ended' then
+			print( "Ball ".. "Colission at X,Y : ".. self.x.. "," ..self.y )
+		end
+	end
+
+	Runtime:addEventListener( "tap", fireCanon )
+	Runtime:addEventListener( "enterFrame", doPlanet )
+	
+end
---
+++ menu.lua
@@ -0,0 +1,57 @@
+module(..., package.seeall)
+
+-- Main function - MUST return a display.newGroup()
+function new()
+	local localGroup = display.newGroup()
+	
+	-- Background
+	local background = display.newImage("images/backgrounds/menu.png")
+	localGroup:insert(background)
+	
+	-- Buttons
+	local singlePlayerButton = display.newImage("images/buttons/singlePlayer.png")
+	local function singlePlayerPressed ( event )
+		if event.phase == "ended" then
+			director:changeScene("game", "fade", "green")
+		end
+	end
+	singlePlayerButton:addEventListener("touch", singlePlayerPressed)
+	singlePlayerButton.x = 160
+	singlePlayerButton.y = 80	
+	localGroup:insert(singlePlayerButton)
+	
+	local singlePlayerButton = display.newImage("images/buttons/singlePlayer.png")
+	local function singlePlayerPressed ( event )
+		if event.phase == "ended" then
+			director:changeScene("game", "fade", "green")
+		end
+	end
+	singlePlayerButton:addEventListener("touch", singlePlayerPressed)
+	singlePlayerButton.x = 160
+	singlePlayerButton.y = 80	
+	localGroup:insert(singlePlayerButton)
+
+	local multiPlayerButton = display.newImage("images/buttons/multiPlayer.png")
+	multiPlayerButton.x = 160
+	multiPlayerButton.y = 130	
+	localGroup:insert(multiPlayerButton)
+	
+	local helpButton = display.newImage("images/buttons/help.png")
+	local function helpPressed ( event )
+		if event.phase == "ended" then
+			director:changeScene("help", "fade", "green")
+		end
+	end
+	helpButton:addEventListener("touch", helpPressed)	
+	helpButton.x = 160
+	helpButton.y = 180	
+	localGroup:insert(helpButton)
+	
+	local aboutButton = display.newImage("images/buttons/about.png")
+	aboutButton.x = 160
+	aboutButton.y = 230	
+	localGroup:insert(aboutButton)	
+					
+	-- MUST return a display.newGroup()
+	return localGroup
+end
